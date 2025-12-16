27cd494a05114fe763ef9e657f79bb81043e8bab
Félix Vittori <vittorifelix@gmail.com>
2025-12-16 13:50:05 +0100

    Fixed the RobotBase class to take a starting position as input in the execute_commands(..) function

--- CleaningRobotAPI/app/core/robot.py
+++ CleaningRobotAPI/app/core/robot.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from CleaningRobotAPI.app.core.grid import Grid
+from app.core.grid import Grid
 
 
 class RobotBase(ABC):
@@ -7,6 +7,7 @@ class RobotBase(ABC):
     def __init__(self, grid : Grid):
         self.grid = grid
         self.position = (0, 0)  # Starting position
+        self.cleaned_tiles = set()
 
     def move(self, dir: str) -> bool:
         if dir == "north":
@@ -23,20 +24,32 @@ class RobotBase(ABC):
             self.position = new_position
             return True
         return False
-    
-    def execute_commands(self, commands: list[str]) -> None:
-        for command in commands:
-            success = self.move(command)
-            if not success:
-                return "error"
-        return "completed"
+
+    def execute_commands(self, commands: list[tuple[str, int]], starting_pos: tuple[int, int]) -> str:
+        # Check that format is correct
+        self.position = starting_pos
+        for direction, steps in commands: 
+            for _ in range(steps):
+                success = self.move(direction)
+                if not success:
+                    return "error", list(self.cleaned_tiles)
+                self.cleaned_tiles.add(self.position)
+        return "completed", list(self.cleaned_tiles)
+               
+    def get_num_cleaned_tiles(self) -> int:
+        return len(self.cleaned_tiles)
             
     def reset_position(self) -> None:
         self.position = (0, 0)
+
+    # @abstractmethod
+    # def detect_dirty(self) -> bool:
+    #     pass
             
 
 class Robot(RobotBase):
     def __init__(self, grid: Grid):
         super().__init__(grid)
-    
+
+ 
 
